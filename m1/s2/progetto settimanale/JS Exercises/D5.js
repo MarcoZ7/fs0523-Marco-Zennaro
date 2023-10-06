e2433406ece3bf21ada610d9cfc838ee47c01d75
MarcoZ7 <marcozennaro87@gmail.com>
2023-10-06 17:02:07 +0200

    progetto 2

--- m1/s2/progetto settimanale/JS Exercises/D5.js
+++ m1/s2/progetto settimanale/JS Exercises/D5.js
@@ -12,14 +12,14 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
-for (let i = 0; i <pets.length;i++);{
+for (let i = 0; i <pets.length;i++){
   console.log(pets[i]);
   }
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
-pets.sort[pets];
+pets.sort();
 
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
@@ -69,17 +69,25 @@ console.log(cars);
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
 let auto = {
-  brand: 'Toyota'
+  brand: 'Fiat',
+  model: 'Stelvio',
+  color: 'blue',
+  trims: ['allure', 'GT'],
+  lincensePlate: 'GG569PK',
 }
+cars.push(auto),
+console.log(cars),
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
+
 const justTrims = []
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -88,7 +96,12 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
 
+let x = 32;
 
+while (x != 32){
+console.log(x);
+x++
+}
 
   
 
@@ -101,4 +114,21 @@ const numericArray = [
 */
 const charactersArray = ['g', 'n', 'u', 'z', 'd']
 
-
+switch(charactersArray){
+  case 'g':
+    console.log(7);
+    break;
+    case 'n':
+    console.log(12);
+    break;
+    case 'u':
+    console.log(19);
+    break;
+     case 'z':
+    console.log(21);
+    break;
+    case 'd':
+    console.log(4);
+    break;
+default:
+}
